61501f6ff4f32c0c1a9590a8b3a833039fca4e4b
Lorenzo Paderi <lorenzo@mijorus.it>
2023-06-27 12:31:52 +0200

    wip

--- extension.js
+++ extension.js
@@ -14,30 +14,16 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
+ * 
+ * Author: Lorenzo Paderi
  */
 
-/* exported init */
-// import { DBus, DBusExportedObject, DBusSignalFlags, Settings } from '@gi-types/gio2';
-// import { PRIORITY_DEFAULT, Source, SOURCE_REMOVE, timeout_add } from '@gi-types/glib2';
-// import { Global } from '@gi-types/shell0';
-// import { SettingsMenu } from '@pano/components/indicator/settingsMenu';
-// import { PanoWindow } from '@pano/containers/panoWindow';
-// import { ClipboardContent, clipboardManager, ContentType } from '@pano/utils/clipboardManager';
-// import { db } from '@pano/utils/db';
-// import { KeyManager } from '@pano/utils/keyManager';
-// const utils = Me.imports.utils;
-
+const { GLib, Gio, Gdk, Clutter } = imports.gi;
 
 class Extension {
     constructor() {
-        console.log('extension initialized');
-
-        const iface = this.loadInterface();
-        this.dbus = imports.gi.Gio.DBusExportedObject.wrapJSObject(iface, this);
-        this.dbus.export(DBus.session, '/it/mijorus/smile');
-
+        // console.log('extension initialized');
         this.virtualKeyboard = undefined;
-        this.timeoutId = undefined;
     }
 
     getVirtualKeyboard() {
@@ -45,52 +31,51 @@ class Extension {
             return this.virtualKeyboard;
         }
 
-        deviceType = imports.gi.Clutter.InputDeviceType.KEYBOARD_DEVICE;
-        this.virtualKeyboard = imports.Clutter.get_default_backend().get_default_seat().create_virtual_device(deviceType);
+        let deviceType = Clutter.InputDeviceType.KEYBOARD_DEVICE;
+        this.virtualKeyboard = Clutter.get_default_backend().get_default_seat().create_virtual_device(deviceType);
 
         return this.virtualKeyboard;
     }
 
     pasteEmoji(text) {
-        let clipboard = imports.gi.Gdk.Clipboard();
+        // let clipboard = Gdk.Clipboard();
 
-        if (clipboard.get_content().get_value() !== text) {
-            let contextProvider = imports.gi.Gdk.ContentProvider.new_for_value(text);
-            clipboard.set_value(contextProvider);
-        }
+        // if (clipboard.get_content().get_value() !== text) {
+        //     let contextProvider = Gdk.ContentProvider.new_for_value(text);
+        //     clipboard.set_value(contextProvider);
+        // }
+        const eventTime = Clutter.get_current_event_time() * 1000;
 
-        this.timeoutId = imports.gi.GLib.timeout_add(PRIORITY_DEFAULT, 250, () => {
-            this.getVirtualKeyboard().notify_keyval(get_current_event_time(), KEY_Control_L, KeyState.RELEASED);
-            this.getVirtualKeyboard().notify_keyval(get_current_event_time(), KEY_Control_L, KeyState.PRESSED);
-            this.getVirtualKeyboard().notify_keyval(get_current_event_time(), KEY_v, KeyState.PRESSED);
-            this.getVirtualKeyboard().notify_keyval(get_current_event_time(), KEY_Control_L, KeyState.RELEASED);
-            this.getVirtualKeyboard().notify_keyval(get_current_event_time(), KEY_v, KeyState.RELEASED);
+        this.timeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 250, () => {
+            this.getVirtualKeyboard().notify_keyval(eventTime, Clutter.KEY_Control_L, Clutter.KeyState.RELEASED);
+            this.getVirtualKeyboard().notify_keyval(eventTime, Clutter.KEY_Control_L, Clutter.KeyState.PRESSED);
+            this.getVirtualKeyboard().notify_keyval(eventTime, Clutter.KEY_v, KeyState.PRESSED);
+            this.getVirtualKeyboard().notify_keyval(eventTime, Clutter.KEY_Control_L, Clutter.KeyState.RELEASED);
+            this.getVirtualKeyboard().notify_keyval(eventTime, Clutter.KEY_v, Clutter.KeyState.RELEASED);
             if (this.timeoutId) {
-                imports.gi.GLib.Source.remove(this.timeoutId);
+                GLib.Source.remove(this.timeoutId);
             }
 
             this.timeoutId = undefined;
         });
     }
 
-    loadInterface() {
-        const uri = `file:///${imports.misc.extensionUtils.getCurrentExtension().path}/dbus-interface.xml`;
-        const file = imports.gi.Gio.File.new_for_uri(uri);
-
-        try {
-            const [, bytes] = file.load_contents(null);
-            return imports.byteArray.toString(bytes);
-        } catch (e) {
-            console.log(`Failed to load D-Bus interface`);
-        }
-
-        return null;
-    }
-
     enable() {
+        this.systemdSignalId = Gio.DBus.session.signal_subscribe(
+            null,
+            'it.mijorus.smile',
+            'CopiedEmoji',
+            '/it/mijorus/smile',
+            null,
+            Gio.DBusSignalFlags.NONE,
+            () => this.pasteEmoji(),
+        );
     }
 
     disable() {
+        if (this.loop) {
+            this.loop.quit();
+        }
     }
 }
 
